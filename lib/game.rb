865c9067f67c0994f27454cfba8b2251bd978dfc
Colin Armstrong <colinwarmstrong@gmail.com>
2018-05-21 18:24:17 -0600

    Adds a start game sequence with intro, play, insructions, and quit options.

--- lib/game.rb
+++ lib/game.rb
@@ -12,4 +12,44 @@ class Game
     @start_time = 0
   end
 
+  def intro
+    print `clear`
+    puts "Welcome to BATTLESHIP\n\n"
+    start_game
+  end
+
+  def start_game
+    puts 'Would you like to (p)lay, read the (i)nstructions, or (q)uit?'
+    start_game_choice = gets_input
+    start_game_flow(start_game_choice)
+  end
+
+  def gets_input
+    print '> '
+    gets.strip.downcase
+  end
+
+  def start_game_flow(start_game_choice)
+    if start_game_choice == 'p' || start_game_choice == 'play'
+      computer_ship_placement
+    elsif start_game_choice = 'i' || start_game_choice == 'instructions'
+      instructions
+    elsif start_game_choice == 'q' || start_game_choice == 'quit'
+      quit
+    else
+      puts 'Invalid choice. Try again.'
+      start_game
+    end
+  end
+
+  def instructions
+    puts 'Here are the instructions.'
+  end
+
+  def quit
+    print `clear`
+    puts 'Goodbye.'
+    exit
+  end
+
 end
