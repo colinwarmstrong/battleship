502188b5be6ad13ed4e3d88a25bbdf31c3a35a5f
Colin Armstrong <colinwarmstrong@gmail.com>
2018-05-22 20:34:28 -0600

    Improves tests for the board class. Adds tests for checking initial format and the content of each element in the board.

--- test/board_test.rb
+++ test/board_test.rb
@@ -1,13 +1,34 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/board.rb'
+require './lib/space.rb'
 
 class BoardTest < Minitest::Test
 
-  def test_it_exists
+  def test_board_exists
     board = Board.new
 
     assert_instance_of Board, board
   end
+
+  def test_board_initializes_as_an_empty_four_by_four_grid
+    board = Board.new
+
+    assert_equal 4, board.grid.length
+    assert_equal 4, board.grid[0].length
+    assert_equal 4, board.grid[1].length
+    assert_equal 4, board.grid[2].length
+    assert_equal 4, board.grid[3].length
+  end
+
+  def test_each_element_of_the_grid_is_an_instance_of_the_space_class
+    board = Board.new
+
+    assert_instance_of Space, board.grid[0][0]
+    assert_instance_of Space, board.grid[3][3]
+    assert_instance_of Space, board.grid[1][2]
+    assert_instance_of Space, board.grid[2][3]
+  end
+
 
 end
